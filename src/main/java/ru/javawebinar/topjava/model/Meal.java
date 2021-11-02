00495764a7a1b4d32563c439f1f8cb880046a5c1
ShumilovRY <donkarleone08962@gmail.com>
2021-11-02 23:27:12 +0300

    first review

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -37,7 +37,7 @@ public class Meal extends AbstractBaseEntity {
     private int calories;
 
     //  @NotNull
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
