79ad3bdcfc243ee728cd986e71a188fca26347ff
ShumilovRY <donkarleone08962@gmail.com>
2021-11-02 23:26:10 +0300

    first review

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,28 +1,38 @@
 package ru.javawebinar.topjava.repository.jpa;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
+    @PersistenceContext
+    private EntityManager em;
+
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
         return null;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
         return false;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+            Meal meal = em.find(Meal.class, id);
+            return (meal == null || meal.getUser().getId() != userId) ? null : meal;
     }
 
     @Override
